4d31e9af229957fc37372d1080e38ea849b0f4ce
unknown <Administrateur@42SE34-CG0455J.ad.campus-eni.fr>
2020-10-06 14:21:22 +0200

    functioning TP1

--- Trivial_Pursuit/src/app/home/home.page.ts
+++ Trivial_Pursuit/src/app/home/home.page.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Error } from "../utils/app-errors-enum"
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
+
+export class HomePage implements OnInit{
+  
+  public username:string ='';
+  public levelMode:string ='';
+  public errors = [];
+  public isVisible:boolean = true; 
   constructor() {}
 
+  ngOnInit(){
+    this.isVisible = true;
+  }
+
+  public startGame() {
+    if(!this.username || this.username.length < 3) {
+      this.errors.push(Error.Username);
+      return;
+    }
+    if(!this.levelMode) {
+      this.errors.push(Error.Level);
+      return;
+    }
+
+    this.showQuestion();
+
+  }
+
+  private showQuestion() {
+    this.isVisible = false;
+  }
 }
---
+++ Trivial_Pursuit/src/app/utils/app-errors-enum.ts
@@ -0,0 +1,4 @@
+export enum Error {
+    Username = "Veuillez saisir un pseudo de 3 caractères au minimum",
+    Level = "Veuillez sélectionner un niveau de difficulté",
+}
