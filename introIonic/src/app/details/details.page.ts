97e904c7266e4aa5d1e5614da73e37ced86d3f71
unknown <Administrateur@42SE34-CG0455J.ad.campus-eni.fr>
2020-10-09 09:50:44 +0200

    end of demo navigation

--- introIonic/src/app/details/details.page.ts
+++ introIonic/src/app/details/details.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
@@ -6,8 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.page.scss'],
 })
 export class DetailsPage implements OnInit {
-
-  constructor() { }
+  public id:string;
+  constructor(private route : ActivatedRoute) {
+    this.route.params.subscribe((params)=>{
+      this.id = params['id'];
+      console.log("identifiant du film " + this.id);
+    });
+    }
+   
 
   ngOnInit() {
   }
--- introIonic/src/app/home/home.page.ts
+++ introIonic/src/app/home/home.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor() {}
+  private subscription : Subscription
+
+  constructor(private router : Router) {}
+
+  ngOnInit(){
+    this.subscription =  this. router.events.subscribe((event)=> {
+      console.log("subscription", event); //permet de voir les éléments de navigation
+    }
+    );
+  }
 
 }
